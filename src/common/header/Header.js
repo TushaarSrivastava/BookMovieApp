3f96553c7429eea914d367d4932fd4646fcb78ee
Tushaar <tushaar.sri98@gmail.com>
2022-11-04 11:58:24 +0530

    integration modal

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,27 +1,97 @@
-import React from "react";
-import { Button } from "@material-ui/core";
+import React, { Fragment, useState } from "react";
+import { Button, Tab, Tabs, TextField } from "@material-ui/core";
 import Logo from "../../assets/logo.svg";
 import { Link } from "react-router-dom";
+import Modal from "react-modal";
+import TabPanel from "../tabPanel/TabPanel";
 import "./Header.css";
 
 const Header = () => {
+  const [loginOpen, setLoginOpen] = useState(false);
+  const [value, setValue] = useState(0);
+
+  const loginHandler = () => {
+    setLoginOpen(true);
+  };
+
+  const closeLoginHandler = () => {
+    setLoginOpen(false);
+  };
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <div className="header">
-      <Link to="/">
-        <img className="logo" src={Logo} alt="logo" />
-      </Link>
-      <div className="button-group">
-        <Button variant="contained" name="Login">
-          Login
-        </Button>
-        <Button variant="contained" name="Book Show" color="primary">
-          Book Show
-        </Button>
-        <Button variant="contained" name="Logout">
-          Logout
-        </Button>
+    <Fragment>
+      <div className="header">
+        <Link to="/">
+          <img className="logo" src={Logo} alt="logo" />
+        </Link>
+        <div className="button-group">
+          <Button variant="contained" name="Login" onClick={loginHandler}>
+            Login
+          </Button>
+          <Button variant="contained" name="Book Show" color="primary">
+            Book Show
+          </Button>
+          <Button variant="contained" name="Logout">
+            Logout
+          </Button>
+        </div>
       </div>
-    </div>
+      <Modal
+        isOpen={loginOpen}
+        ariaHideApp={false}
+        style={{
+          overlay: {
+            position: "fixed",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundColor: "rgba(255, 255, 255, 0.75)",
+          },
+          content: {
+            position: "absolute",
+            top: "40px",
+            left: "500px",
+            right: "500px",
+            bottom: "40px",
+            width: "300px",
+            border: "1px solid #ccc",
+            background: "#fff",
+            overflow: "auto",
+            WebkitOverflowScrolling: "touch",
+            borderRadius: "4px",
+            outline: "none",
+            padding: "20px",
+          },
+        }}
+      >
+        <Tabs value={value} onChange={handleChange}>
+          <Tab label="Login" />
+          <Tab label="Register" />
+        </Tabs>
+        <TabPanel value={value} index={0}>
+          <TextField label="Email" required />
+          <TextField label="Password" required type="password" />
+          <Button onClick={closeLoginHandler} color="primary">
+            Close Modal
+          </Button>
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <TextField label="First Name" required />
+          <TextField label="Last Name" />
+          <TextField label="Email" required />
+          <TextField label="Password" required type="password" />
+          <TextField label="Contact No" required />
+          <Button onClick={closeLoginHandler} color="primary">
+            Close Modal
+          </Button>
+        </TabPanel>
+      </Modal>
+    </Fragment>
   );
 };
 
